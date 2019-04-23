0df1925082b97abd9a4a95ee922ad8dae61f8447
john2796 <jbmiranda22796@gmail.com>
2019-04-22 18:59:17 -0700

    Working on Arguments and Keyword Arguments

--- src/args.py
+++ src/args.py
@@ -18,6 +18,14 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
+
+def f2(*num):
+    sum = 0
+    for n in num:
+        sum = sum + n
+    return sum
+
+
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -26,6 +34,15 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
+
+# Answer
+# def f2(*num):
+#     x = 0
+#     for n in num:
+#         x += sum(n)
+#     return x
+
+
 print(f2(a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
